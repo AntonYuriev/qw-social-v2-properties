8e2499bbf708cad9e1fbabe3b8125cf6befb268d
a.yurev <a.yurev@qiwi.com>
2018-12-14 21:52:06 +0300

    message

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
 import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
 
 /*
 The settings script is an entry point for defining a TeamCity
