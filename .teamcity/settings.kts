85c2f010c5bc6297b4fd17a27c01a0796c2640ea
a.yurev <a.yurev@qiwi.com>
2018-12-15 10:56:17 +0300

    message

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -4,6 +4,8 @@ import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
 
 version = "2018.2"
 
+val applicationName = "qw-social-v2"
+
 project {
 
     buildType(Build)
@@ -22,8 +24,8 @@ object Build : BuildType({
                 curl \
                     -H "X-Consul-Token: d449e42a-f011-a4ff-b7b0-efa0d54022e7" \
                     --request PUT \
-                    --data "@%teamcity.build.checkoutDir%/resources/qw-social-v2-production.yml" \
-                    http://127.0.0.1:8500/v1/kv/config/applications/qw/qw-social-v2-production.yml
+                    --data "@%teamcity.build.checkoutDir%/resources/$applicationName-dev.yml" \
+                    http://127.0.0.1:8500/v1/kv/config/applications/qw/qw-social-v2-dev.yml
             """
         }
     }
