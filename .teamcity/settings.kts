898a5b51f850f3c86ca217db9ae9e8d92c366f3c
a.yurev <a.yurev@qiwi.com>
2018-12-15 11:00:51 +0300

    message

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -12,6 +12,7 @@ project {
 object Build : BuildType({
     name = "Build"
 
+    val acl = "d449e42a-f011-a4ff-b7b0-efa0d54022e7"
     val applicationName = "qw-social-v2"
 
     vcs {
@@ -22,10 +23,30 @@ object Build : BuildType({
         script {
             scriptContent = """
                 curl \
-                    -H "X-Consul-Token: d449e42a-f011-a4ff-b7b0-efa0d54022e7" \
+                    -H "X-Consul-Token: $acl" \
                     --request PUT \
                     --data "@%teamcity.build.checkoutDir%/resources/$applicationName-dev.yml" \
-                    http://127.0.0.1:8500/v1/kv/config/applications/qw/qw-social-v2-dev.yml
+                    http://127.0.0.1:8500/v1/kv/config/applications/qw/$applicationName-dev.yml
+            """
+        }
+
+        script {
+            scriptContent = """
+                curl \
+                    -H "X-Consul-Token: $acl" \
+                    --request PUT \
+                    --data "@%teamcity.build.checkoutDir%/resources/$applicationName-testing.yml" \
+                    http://127.0.0.1:8500/v1/kv/config/applications/qw/$applicationName-testing.yml
+            """
+        }
+
+        script {
+            scriptContent = """
+                curl \
+                    -H "X-Consul-Token: $acl" \
+                    --request PUT \
+                    --data "@%teamcity.build.checkoutDir%/resources/$applicationName-stage.yml" \
+                    http://127.0.0.1:8500/v1/kv/config/applications/qw/$applicationName-stage.yml
             """
         }
     }
